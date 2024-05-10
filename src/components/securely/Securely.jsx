06b64b0670a6a1781e62adb05f24783a8d4ab42d
Ayodejioladimeji <layobright11@gmail.com>
2024-05-10 21:13:19 +0100

    fixed securely

--- src/components/securely/Securely.jsx
+++ src/components/securely/Securely.jsx
@@ -7,15 +7,17 @@ import ProductCard from "@/common/productcard/ProductCard";
 const Securely = () => {
   return (
     <div className="secure">
-      <Heading
-        heading="Shop with Confidence and Convenience"
-        text="Enjoy a seamless shopping experience with our easy-to-use online store. Browse our curated collections, choose your favorite styles, and check out with ease."
-      />
+      <div className="container">
+        <Heading
+          heading="Shop with Confidence and Convenience"
+          text="Enjoy a seamless shopping experience with our easy-to-use online store. Browse our curated collections, choose your favorite styles, and check out with ease."
+        />
 
-      <div className="secure-box">
-        {data?.slice(0, 6)?.map((item, key) => {
-          return <Card item={item} key={key} />;
-        })}
+        <div className="secure-box">
+          {data?.slice(0, 8)?.map((item, key) => {
+            return <Card item={item} key={key} />;
+          })}
+        </div>
       </div>
     </div>
   );
