26580ca08e9cb03b9c8b2b2bfd9f173e616e3154
Ayodejioladimeji <layobright11@gmail.com>
2024-06-02 22:05:49 +0100

    restructured footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,49 +14,70 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="container">
-        <div className="footer-cards">
-          <div className="footer-logo">
-            <Image
-              src="/images/logos.svg"
-              alt=""
-              width={100}
-              height={100}
-              unoptimized
-            />
-          </div>
+        <div className="row">
+          <div className="col-6">
+            <div className="footer-cards">
+              <div className="footer-logo">
+                <Image
+                  src="/images/logo.svg"
+                  alt=""
+                  width={100}
+                  height={100}
+                  unoptimized
+                />
+              </div>
 
-          <p>Stay trendy, stay you</p>
+              <p>Stay trendy, stay you</p>
+              <small>Visit our Store to discover the perfect fabrics for your next occasion.</small><br />
+              <small>Follow us on social media to stay updated on the latest trends,
+                new arrivals, and special promotions.</small>
 
-          <div className="footer-content">
-            {/* <p>
+              <div className="footer-content">
+                {/* <p>
               Follow us on social media to stay updated on the latest trends,
               new arrivals, and special promotions. Join our community and be
               the first to know about exclusive offers and events.
             </p> */}
-            <div className="footer-icons">
-              <a
-                href="/https://www.facebook.com/Verifibiz_africa-108734885270817/"
-                target="_blank"
-              >
-                <FaFacebookSquare className="footer-social" />
-              </a>
-              <a
-                href="/https://www.instagram.com/verifibiz_africa/"
-                target="_blank"
-              >
-                <FaInstagram className="footer-social" />
-              </a>
-              <a href="/" target="_blank">
-                <FaTwitterSquare className="footer-social" />
-              </a>
+                <div className="footer-icons">
+                  <a
+                    href="/https://www.facebook.com/Verifibiz_africa-108734885270817/"
+                    target="_blank"
+                  >
+                    <FaFacebookSquare className="footer-social" />
+                  </a>
+                  <a
+                    href="/https://www.instagram.com/verifibiz_africa/"
+                    target="_blank"
+                  >
+                    <FaInstagram className="footer-social" />
+                  </a>
+                  <a href="/" target="_blank">
+                    <FaTwitterSquare className="footer-social" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-3">
+            <div className="footer-cards">
+              <h3>Shop Location</h3>
+              <p>09037102710</p>
+              <small>Ibafo, Ogun State, Nigeria.</small>
+            </div>
+          </div>
+          <div className="col-3">
+            <div className="footer-cards">
+              <h3>Shop Location</h3>
+              <p>09037102710</p>
+              <small>Ibafo, Ogun State, Nigeria.</small>
             </div>
           </div>
         </div>
 
+        <small className="footer-bottom">
+          copyright &copy; {new Date().getFullYear()} All rights reserved.
+        </small>
       </div>
-      <small className="footer-rights">
-        copyright &copy; {new Date().getFullYear()} All rights reserved.
-      </small>
     </div>
   );
 };
